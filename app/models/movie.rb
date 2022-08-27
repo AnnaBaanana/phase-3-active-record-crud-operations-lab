dece514d4f3367a5497bb7a57f69bd16d442eea3
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-08-27 15:45:11 -0400

    commit tree bfa609a40862dbd6107a18bbdd39a3cdcf7197ed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,48 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title (title)
+        self.create( title: title)
+    end
+
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id (id)
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes (attributes)
+        self.where(attributes)
+    end
+
+    def self.find_movies_after_2002
+        self.where(release_date: "2002")
+    end
+
+    def update_with_attributes (attr, new_val)
+        self.update_attribute(:attr, new_val)
+    end
+
+    def self.update_all_titles (attribute, value)
+        self.update(attribute: value)
+    end
+
+    def self.delete_by_id (id)
+        Movie.find(id).destroy
+
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+    end
+
 end
